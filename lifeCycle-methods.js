6dae913935f03271b9396f2816086ec7260365e7
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-04-18 12:54:48 +0530

    👍
    lifeCycle-methods exploration.

---
+++ lifeCycle-methods.js
@@ -0,0 +1,13 @@
+//life Cycle methods  are methods only on class.
+//componentDidMount(), componentDidUpdate(), componentWillUnmount(). 
+//can access this.state inside it and extra features like prevState, prevProps.
+//for further ref react website docs.
+
+//order of the attributes important.
+/*
+componentDidUpdate(prevProps,prevState ){
+    if(prevState.options.length !== this.state.options.length) {
+        console.log('correct');
+    };
+}
+*/
