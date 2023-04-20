3ead494114bef6f554446b2509fb8abb09971ac3
Meenakshi-Sivakumar <meenakshisivakumar08@gmail.com>
2023-04-20 13:47:33 +0530

    👍

---
+++ pass-state-as-props-to-child-comp.js
@@ -0,0 +1,31 @@
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'CamperBot'
+    }
+  }
+  render() {
+    return (
+       <div>
+         {/* Change code below this line */}
+         <Navbar name={this.state.name}/>
+         {/* Change code above this line */}
+       </div>
+    );
+  }
+};
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+    <div>
+      {/* here */}
+      <h1>Hello, my name is: {this.props.name} </h1>
+    </div>
+    );
+  }
+};
